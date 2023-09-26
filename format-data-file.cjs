e825c11c8bd2fa9fd9346c2552a314ddfa8ff210
tomrcc <tom@cloudcannon.com>
2023-09-26 15:40:18 +1300

    simplify data download

--- format-data-file.cjs
+++ format-data-file.cjs
@@ -8,22 +8,19 @@ const path = require('path');
     throw new Error('Please provide the collection name to install')
   }
 
-  // rm -rf ./data/temp-$COLLECTION
-  // mkdir ./data/temp-$COLLECTION
-  // cp ./great-lakes-data/content/$COLLECTION/* ./data/temp-$COLLECTION
   console.log(`📝 Formatting ${collectionName} data files now...`);
-  const collectionDir = path.join('.' , 'data', `temp-${collectionName}`);
+  const collectionDir = path.join('great-lakes-data', 'content', collectionName);
   const files = await fs.readdir(collectionDir);
 
   const items = await Promise.all(files.map(async (fileName) => {
-    const dataFilepath = `${collectionDir}/${fileName}`;
+    const dataFilepath = path.join(collectionDir, fileName);
     console.log(`📝 Reading ${dataFilepath} into data file...`);
     const contents = await fs.readFile(dataFilepath);
     await fs.unlink(dataFilepath);
     return JSON.parse(contents.toString('utf8'));
   }));
 
-  const destPath = `./data/${collectionName}.json`;
+  const destPath = path.join('data', `${collectionName}.json`);
   console.log(`Writing to ${destPath}`);
   await fs.writeFile(destPath, JSON.stringify(items, null, '\t'), {
     encoding: 'utf8',
--- install-data-file.sh
+++ install-data-file.sh
@@ -1,14 +1,4 @@
 # exit when any command fails
 set -e
 
-if [ "$1" = "" ]
-then
-  echo "Usage: Please provide the collection name to install into a datafile"
-  exit
-fi
-
-COLLECTION=$1
-rm -rf ./data/temp-$COLLECTION
-mkdir ./data/temp-$COLLECTION
-cp ./great-lakes-data/content/$COLLECTION/* ./data/temp-$COLLECTION
-node ./great-lakes-data/format-data-file.cjs $COLLECTION
+node ./great-lakes-data/format-data-file.cjs $1
