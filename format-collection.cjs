45ccc7c00d116e255e535e3d158ca8185da56d19
tomrcc <tom@cloudcannon.com>
2023-09-27 15:19:00 +1300

    added mkdirp

--- format-collection.cjs
+++ format-collection.cjs
@@ -6,36 +6,47 @@ const YAML = require('json-to-pretty-yaml');
   const collectionName = process.argv[2];
 
   if (!collectionName) {
-    throw new Error('Please provide the collection name to install')
+    throw new Error('Please provide the collection name to install');
   }
 
-  const collectionDir = path.join('great-lakes-data', 'content', collectionName);
-  const destDir = path.join('.' , 'src', 'content', collectionName);
-  console.log(`📝 Formatting ${collectionName} data files now from ${collectionDir}...`);
+  const collectionDir = path.join(
+    'great-lakes-data',
+    'content',
+    collectionName
+  );
+  const destDir = path.join('.', 'src', 'content', collectionName);
+  await fs.mkdir(destDir, { recursive: true });
+  console.log(
+    `📝 Formatting ${collectionName} data files now from ${collectionDir}...`
+  );
   const files = await fs.readdir(collectionDir);
 
-  await Promise.all(files.map(async (filename) => {
-    // Parse the filename
-    const originalFilepath = path.join(collectionDir, filename);
-    const markdownFilename = filename.replace(/json/g, 'md').replace(/_/g, '-');
-    const markdownFilepath = path.join(destDir, markdownFilename);
+  await Promise.all(
+    files.map(async (filename) => {
+      // Parse the filename
+      const originalFilepath = path.join(collectionDir, filename);
+      const markdownFilename = filename
+        .replace(/json/g, 'md')
+        .replace(/_/g, '-');
+      const markdownFilepath = path.join(destDir, markdownFilename);
 
-    // Parse JSON data file and turn the front matter into formatted yaml
-    console.log(`📝 Reading ${originalFilepath}...`);
-    const contents = await fs.readFile(originalFilepath);
-    // console.log(originalFilepath, contents.toString('utf8'));
-    const jsonObj = JSON.parse(contents.toString('utf8'))
-    const { content_markdown, ...frontMatterData } = jsonObj;
+      // Parse JSON data file and turn the front matter into formatted yaml
+      console.log(`📝 Reading ${originalFilepath}...`);
+      const contents = await fs.readFile(originalFilepath);
+      // console.log(originalFilepath, contents.toString('utf8'));
+      const jsonObj = JSON.parse(contents.toString('utf8'));
+      const { content_markdown, ...frontMatterData } = jsonObj;
 
-    // Add the markdown content underneath the formatted yaml
-    const totalPage = [
-      '---',
-      YAML.stringify(frontMatterData),
-      '---',
-      content_markdown
-    ].join('\n');
-    
-    // Create new markdown file
-    return fs.writeFile(markdownFilepath, totalPage)
-  }));
+      // Add the markdown content underneath the formatted yaml
+      const totalPage = [
+        '---',
+        YAML.stringify(frontMatterData),
+        '---',
+        content_markdown,
+      ].join('\n');
+
+      // Create new markdown file
+      return fs.writeFile(markdownFilepath, totalPage);
+    })
+  );
 })();
--- format-data-file.cjs
+++ format-data-file.cjs
@@ -5,23 +5,30 @@ const path = require('path');
   const collectionName = process.argv[2];
 
   if (!collectionName) {
-    throw new Error('Please provide the collection name to install')
+    throw new Error('Please provide the collection name to install');
   }
 
   console.log(`📝 Formatting ${collectionName} data files now...`);
-  const collectionDir = path.join('great-lakes-data', 'content', collectionName);
+  const collectionDir = path.join(
+    'great-lakes-data',
+    'content',
+    collectionName
+  );
   const files = await fs.readdir(collectionDir);
 
-  const items = await Promise.all(files.map(async (fileName) => {
-    const dataFilepath = path.join(collectionDir, fileName);
-    console.log(`📝 Reading ${dataFilepath} into data file...`);
-    const contents = await fs.readFile(dataFilepath);
-    await fs.unlink(dataFilepath);
-    return JSON.parse(contents.toString('utf8'));
-  }));
+  const items = await Promise.all(
+    files.map(async (fileName) => {
+      const dataFilepath = path.join(collectionDir, fileName);
+      console.log(`📝 Reading ${dataFilepath} into data file...`);
+      const contents = await fs.readFile(dataFilepath);
+      await fs.unlink(dataFilepath);
+      return JSON.parse(contents.toString('utf8'));
+    })
+  );
 
   const destPath = path.join('data', `${collectionName}.json`);
   console.log(`Writing to ${destPath}`);
+  await fs.mkdir(path.join('data'), { recursive: true });
   await fs.writeFile(destPath, JSON.stringify(items, null, '\t'), {
     encoding: 'utf8',
   });
